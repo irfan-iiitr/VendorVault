5a2cfdd91ec9144ab2a59f8416aebda9fbd1b8c0
irfan-iiitr <imirfansari@gmail.com>
2024-07-25 10:25:12 +0530

    Orders Added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,12 @@ import authRoutes from './auth'
 import productsRoutes from './product';
 import usersRoutes from './users';
 import cartRoutes from './cart';
+import orderRoutes from './order';
 
 rootRouter.use('/auth',authRoutes);
 rootRouter.use('/products',productsRoutes);
 rootRouter.use('/users', usersRoutes )
 rootRouter.use('/carts', cartRoutes)
+rootRouter.use('/orders', orderRoutes)
 
 export default rootRouter;
